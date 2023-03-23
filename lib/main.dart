f868297c35b63013eed7aa2f603ff87ddb4894c6
Henrique Cisi <henrique.cisi@accenture.com>
2023-03-23 08:56:52 -0300

    adds initial structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
-
 import 'package:flutter/material.dart';
+import 'package:project_kitchen_recipes/screens/categories_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -13,32 +13,15 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Vamos Cozinhar?',
       theme: ourTheme.copyWith(
-          colorScheme: ourTheme.colorScheme.copyWith(
-              primary:  Colors.amber.shade800),
+          colorScheme:
+              ourTheme.colorScheme.copyWith(primary: Colors.pink),
           textTheme: ourTheme.textTheme.copyWith(
-              titleLarge:
-                   const TextStyle(fontSize: 22, fontWeight: FontWeight.bold, color: Colors.black))),
-      home: const MyHomePage(),
+            titleLarge: const TextStyle(
+                fontSize: 20, color: Colors.white, fontFamily: 'Raleway', fontWeight: FontWeight.bold),
+            bodyLarge: const TextStyle(
+                fontSize: 22, color: Colors.black, fontFamily: 'RobotoCondensed'),
+          )),
+      home: const CategoriesScreen(),
     );
   }
 }
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-          title: Text('Vamos Cozinhar?',
-              style: Theme.of(context).textTheme.titleLarge)),
-      body: const Center(child: Text('Navegar é preciso')),
-    );
-  }
-}
-
