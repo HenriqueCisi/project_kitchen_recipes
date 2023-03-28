80baaf3e4033cb6cd8b02c232dd5cdca8bd3171d
Henrique Cisi <henrique.cisi@accenture.com>
2023-03-28 20:42:27 -0300

    adds floatingActionButton + pop navigation with params

--- lib/screens/meal_detail_screen.dart
+++ lib/screens/meal_detail_screen.dart
@@ -74,6 +74,10 @@ class MealDetailScreen extends StatelessWidget {
               )
             ],
           ),
-        ));
+        ),
+        floatingActionButton: FloatingActionButton(child: const Icon(Icons.star), onPressed: (){
+          Navigator.of(context).pop(meal.title);
+        },),
+      );
   }
 }
