996407723351da27edc72fd54fd11c58652fa0c6
Henrique Cisi <henrique.cisi@accenture.com>
2023-03-27 21:08:11 -0300

    adds ingredients and steps section

--- lib/screens/meal_detail_screen.dart
+++ lib/screens/meal_detail_screen.dart
@@ -5,17 +5,75 @@ import '../models/meal.dart';
 class MealDetailScreen extends StatelessWidget {
   const MealDetailScreen({super.key});
 
+  Widget _createSectionTitle(BuildContext context, String title) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 10),
+      child: Text(title, style: Theme.of(context).textTheme.bodyLarge),
+    );
+  }
+
+  Widget _createSectionContainer(Widget child) {
+    return Container(
+        width: 350,
+        height: 200,
+        padding: const EdgeInsets.all(10),
+        margin: const EdgeInsets.all(10),
+        decoration: BoxDecoration(
+            color: Colors.white,
+            border: Border.all(color: Colors.grey),
+            borderRadius: BorderRadius.circular(10)),
+        child: child);
+  }
+
   @override
   Widget build(BuildContext context) {
     final meal = ModalRoute.of(context)?.settings.arguments as Meal;
 
     return Scaffold(
-      appBar: AppBar(
-        title: Text(meal.title),
-      ),
-      body: const Center(
-        child: Text('Detalhes da Refeição'),
-      ),
-    );
+        appBar: AppBar(
+          title: Text(meal.title),
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 300,
+                width: double.infinity,
+                child: Image.network(
+                  meal.imageUrl,
+                  fit: BoxFit.cover,
+                ),
+              ),
+              _createSectionTitle(context, 'Ingredients'),
+              _createSectionContainer(
+                ListView.builder(
+                    itemCount: meal.ingredients.length,
+                    itemBuilder: ((context, index) {
+                      return Padding(
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 5, horizontal: 10),
+                        child: Card(
+                            color: Colors.amberAccent,
+                            child: Text(meal.ingredients[index])),
+                      );
+                    })),
+              ),
+              _createSectionTitle(context, 'Steps'),
+              _createSectionContainer(
+                ListView.builder(itemCount: meal.steps.length, itemBuilder: (context, index){
+                  return Column(
+                    children: [
+                      ListTile(
+                        leading: CircleAvatar(child: Text('${index + 1}')),
+                        title: Text(meal.steps[index]),
+                      ),
+                      const Divider()
+                    ],
+                  );
+                },)
+              )
+            ],
+          ),
+        ));
   }
 }
